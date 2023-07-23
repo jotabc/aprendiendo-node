033e455e0ba31ecbce1c94f641cb6c94eaf8f401
Jonnathan <jonnabcl56@gmail.com>
2023-07-23 17:53:52 -0500

    System modules (path) ejercicio ls advanced 2 extraccion de informacion

--- systemModules/path/ls-advanced.js
+++ systemModules/path/ls-advanced.js
@@ -1,19 +1,43 @@
 const fs = require('node:fs/promises')
+const path = require('node:path')
 
 const folder = process.argv[2] ?? '.' 
 
-fs.readdir(folder)
-  .then(files => {
-    files.forEach(file => {
-      console.log(file)
-    })
-  })
-  .catch(err => {
+async function ls(folder) {
+  let files
+
+  try {
+    files = await fs.readdir(folder)
+  } catch (error) {
     // si no manejadmos los errores nos da un error interno de node. triggerUncaughtException este significa que tenmos un error no controlado, porque en un catch nosotros no podemos hacer que nuestro código no reviente.
-    if(err) {
-      console.error('Error al leer el directorio', err)
-      return;
+    console.error(`Error al leer el directorio ${folder}`)
+    process.exit(1)
+  }
+
+  const filesPromises = files.map(async file => {
+    // obtenemos el filePath de cada archivo
+    const filePath = path.join(folder, file)
+    let stats
+    try {
+      // obtenemos el stat de cada uno, stat nos da la infrormación del archivo
+      stats = await fs.stat(filePath)
+    } catch {
+      console.log(`No se puedo obtener el archivo ${filePath}`)
+      process.exit(1)
     }
 
-    // podemos seguir haciendo cosas si qieremos que nuestro proceso no se detenga.
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = stats.size
+    const fileModified = stats.mtime.toLocaleString()
+
+    return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
   })
+
+  const filesInfo = await Promise.all(filesPromises)
+
+  filesInfo.forEach(fileInfo => console.log(fileInfo))
+
+}
+
+ls(folder)
